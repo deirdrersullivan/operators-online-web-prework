6562b9e16bd2173f73fe52c03045429e35d222aa
Deirdre  Sullivan <dsulliv4@binghamton.edu>
2019-04-19 00:31:18 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,5 @@
 def unsafe?(speed)
-  if speed > 60 
+  if speed > 60
     true
   elsif speed < 40
     true
@@ -11,6 +11,3 @@ end
 def not_safe?(speed)
   speed < 40 || speed > 60 ? true : false
 end
-	
-
-
